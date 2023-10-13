450c34c42df16a2b53eaadc3e092deeb7f58ec2e
julianne <julianne@LAPTOP-AM38FBFP>
2023-10-13 22:51:22 +0800

    1st commit

---
+++ src/MainClass.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author julianne
+ */
+public class MainClass {
+    public static void main(String[] args)
+    {
+            new Calculator();
+       
+    }
+}
+
